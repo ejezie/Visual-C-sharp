7f9a080668384b451dee1de0bcf2140c2b15ac12
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-04-06 14:19:19 +0100

    Initial check-in of module Learn

---
+++ Learn/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20 * 2;
+            Console.WriteLine(x);
+        }
+    }
+}
